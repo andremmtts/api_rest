282aefa85b9d122c063c759653e869dd63d97ec8
andremmtts <andremmtts@gmail.com>
2023-06-29 15:49:21 -0300

    'atualizando_porta_de_acesso'

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ import fotoRoutes from './routes/fotoRoutes';
 const whiteList = [
   'http://35.247.207.218:81',
   'http://localhost:3001',
+  'http://localhost:3000',
 ];
 
 const corsOptions = {
